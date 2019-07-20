dc26c6246745b3c68b9104ff88b9cee8528f3f41
Chris <cabiediger@gmail.com>
2019-07-20 11:24:32 -0500

    add content, modify styles, add pics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+       <ul className="icons">
+            <li><a href="https://www.linkedin.com/in/biedswebdev/" className="icon fa-linkedin" target='_blank'><span className="label">LinkedIn</span></a></li>
+            <li><a href="https://github.com/cbied" className="icon fa-github"><span className="label" target='_blank'>GitHub</span></a></li>
+          </ul>
     </footer>
 )
 
