bd1fd0115831cea13045973baa451d5b6d1a8dff
Chris <cabiediger@gmail.com>
2019-07-21 12:55:12 -0500

    update skills styles, add modal for work, fix styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../components/layout'
-
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
